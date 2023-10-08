eead076d62588be4326f2ec909e0dbfd494ccf51
eugenioclrc <eugenioclrc@gmail.com>
2023-10-08 10:56:31 -0300

    verison fix (older)

--- create2deployer/test/Deploy.sol
+++ create2deployer/test/Deploy.sol
@@ -6,7 +6,7 @@ function compile(Vm vm, string memory source) returns (bytes memory) {
     string[] memory cmd = new string[](5);
     cmd[0] = "huffc";
     cmd[1] = "-e";
-    cmd[2] = "shanghai";
+    cmd[2] = "paris";
     cmd[3] = "--bytecode";
     cmd[4] = source;
 
